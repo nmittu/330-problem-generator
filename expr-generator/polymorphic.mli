aa3d7cd29dce561b87b069ddf014595d85ceb32c
Nikhil Mittu <nmittu@users.noreply.github.com>
2023-03-27 17:18:16 -0400

    Fix polymorphic generation (#10)

--- expr-generator/polymorphic.mli
+++ expr-generator/polymorphic.mli
@@ -1,19 +1,30 @@
 open! Core
 include Generator.Generator
 
-type expr =
-  | Var
-  | List of expr list
-  | Cons of expr * expr
-  | Tuple of expr list
-  | App of expr * expr list
-  | Eq of expr * expr
-  | Gt of expr * expr
-  | Lt of expr * expr
-  | Or of expr * expr
-  | And of expr * expr
-  | Not of expr
-  | If of expr * expr * expr
+type func
 
-val generate : int -> expr
-val to_string : expr -> string
+type _ expr =
+  | Var : 'a expr
+  | List : 'a expr list -> 'a list expr
+  | Cons : 'a expr * 'a list expr -> 'a list expr
+  | Tuple : 'a expr * 'b expr -> ('a * 'b) expr
+  | App : func expr * 'a expr list -> 'a expr
+  | Eq : 'a expr * 'a expr -> bool expr
+  | Gt : 'a expr * 'a expr -> bool expr
+  | Lt : 'a expr * 'a expr -> bool expr
+  | Or : bool expr * bool expr -> bool expr
+  | And : bool expr * bool expr -> bool expr
+  | Not : bool expr -> bool expr
+  | If : bool expr * 'a expr * 'a expr -> 'a expr
+
+type polyVal
+
+type _ typ =
+  | AT : polyVal typ
+  | ListT : 'a typ -> 'a list typ
+  | TupleT : 'a typ * 'b typ -> ('a * 'b) typ
+  | FuncT : func typ
+  | BoolT : bool typ
+
+val generate : 'a typ -> int -> 'a expr
+val to_string : 'a expr -> string
